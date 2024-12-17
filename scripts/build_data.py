e4aab63c59e61f096359b7afa42897f332111f1c
Land-Schleswig-Holstein <NHBericht@stk.landsh.de>
2024-12-17 11:40:08 +0100

    Update build_data.py

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -1,8 +1,3 @@
-
 from sdg.open_sdg import open_sdg_build
-
-import warnings
-warnings.filterwarnings("ignore", message="numpy.dtype size changed")
-warnings.filterwarnings("ignore", message="numpy.ufunc size changed") 
 
 open_sdg_build(config='config_data.yml')
