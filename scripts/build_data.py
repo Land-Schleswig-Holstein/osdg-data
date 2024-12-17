9e044c21c96c21539a06e6ae4bf3374be24bd4c5
Land-Schleswig-Holstein <NHBericht@stk.landsh.de>
2024-12-17 10:54:28 +0100

    Update build_data.py

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -1,8 +1,7 @@
 from sdg.open_sdg import open_sdg_build
 
-open_sdg_build(config='config_data.yml')
-
- """Stops the numpy error showing up"""
 import warnings
 warnings.filterwarnings("ignore", message="numpy.dtype size changed")
 warnings.filterwarnings("ignore", message="numpy.ufunc size changed") 
+
+open_sdg_build(config='config_data.yml')
